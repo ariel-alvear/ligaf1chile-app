1c58f675336bd394c7ac6b29a751cb8eb24f1f4c
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-09-22 20:37:59 -0300

    pistas: se crea modelo de pistas y seed de pistas

--- db/seeds/00_users_seed.rb
+++ db/seeds/00_users_seed.rb
@@ -1,28 +1,28 @@
-# Crear usuarios
-User.destroy_all
+# # Crear usuarios
+# User.destroy_all
 
-User.create(
-              [
-                {
-                  email: 'ariel@liga.cl',
-                  password: '12345678',
-                  name: 'ariel',
-                  last_name: 'alvear',
-                  drivers_name: 'aialvear',
-                },
-                {
-                  email: 'juan@liga.cl',
-                  password: '12345678',
-                  name: 'juan',
-                  last_name: 'perez',
-                  drivers_name: 'juano',
-                },
-                {
-                  email: 'pedro@liga.cl',
-                  password: '12345678',
-                  name: 'pedro',
-                  last_name: 'hernandez',
-                  drivers_name: 'peter',
-                },
-              ]
-            )
+# User.create(
+#               [
+#                 {
+#                   email: 'ariel@liga.cl',
+#                   password: '12345678',
+#                   name: 'ariel',
+#                   last_name: 'alvear',
+#                   drivers_name: 'aialvear',
+#                 },
+#                 {
+#                   email: 'juan@liga.cl',
+#                   password: '12345678',
+#                   name: 'juan',
+#                   last_name: 'perez',
+#                   drivers_name: 'juano',
+#                 },
+#                 {
+#                   email: 'pedro@liga.cl',
+#                   password: '12345678',
+#                   name: 'pedro',
+#                   last_name: 'hernandez',
+#                   drivers_name: 'peter',
+#                 },
+#               ]
+#             )
