ee1c4ad37d2c5e0f515c03bb423109718a90a4da
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-09-23 19:55:03 -0300

    usuario: se agregan atributos en formulario de registro, se agregan validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
 
   has_many :user_roles
   has_many :roles, through: :user_roles
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :drivers_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :drivers_name, uniqueness: true
 end
