da5f098839a1936574b9fc97c5fa251bb9aed58b
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-09-29 20:07:44 -0300

    evento: se pueden crear eventos, y se listan los eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :track
-  belongs_to :user
   belongs_to :league
+
+  def track_name
+    Track.find(self.track_id).name
+  end
+
+  def track_country
+    Track.find(self.track_id).country
+  end
 end
