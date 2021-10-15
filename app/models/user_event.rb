ec5d17c4c689cfdf5db750f3cc4325641c11ec0f
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-10-15 01:29:44 -0300

    se crea tabla de evento

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -8,6 +8,14 @@ class UserEvent < ApplicationRecord
     self.user.name
   end
 
+  def user_last_name
+    self.user.last_name
+  end
+
+  def user_driver_name
+    self.user.drivers_name
+  end
+
   def check_for_identical_record
     if UserEvent.exists?(user_id: self.user_id, event_id: self.event_id)
       raise Exception.new('Participantes duplicados!')
