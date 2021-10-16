0d2bf92f30f88547aabc02e87acd28f75842ca4a
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-10-16 18:12:43 -0300

    eventos: se debe crear nombre para el evento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_023923) do
+ActiveRecord::Schema.define(version: 2021_10_16_211042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_023923) do
     t.bigint "league_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["league_id"], name: "index_events_on_league_id"
     t.index ["track_id"], name: "index_events_on_track_id"
   end
