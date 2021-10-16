1d46eb453c052f9e61169cc7c223ee2829900bf8
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-10-16 18:09:23 -0300

    select participantes de evento, solo muestra participantes de la liga

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def users_for_select_event(league_id)
+    UserLeague.where(league_id: league_id).collect { |user_league| [user_league.user.drivers_name, user_league.user_id] }
+  end
 end
