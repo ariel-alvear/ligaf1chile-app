a74ef497bc1af365faa9644743396721dc30da3b
Ariel Alvear <ariel.alvear.l@gmail.com>
2022-03-09 20:54:56 -0300

    Hito 2: Crear home diferenciado para usuarios logeados y visitas

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
 
   get 'configurations/index'
 
-  root 'leagues#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
